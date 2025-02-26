9a774a893a2b2e34cde37c8c0323393e32563708
linqingfeng <shuhao54@outlook.com>
2025-02-26 18:07:50 +0800

    Update rtcConfig.ts

--- src/config/rtcConfig.ts
+++ src/config/rtcConfig.ts
@@ -4,16 +4,35 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection#parameters
 export const rtcConfig: RTCConfiguration = {
   // These are the relay servers that are used in case a direct peer-to-peer
-  // connection cannot be made. Feel free to change them as you'd like. If you
-  // would like to disable relay servers entirely, remove the `iceServers`
-  // property from the rtcConfig object. IF YOU DISABLE RELAY SERVERS,
-  // CHITCHATTER PEERS MAY NOT BE ABLE TO CONNECT DEPENDING ON HOW THEY ARE
-  // CONNECTED TO THE INTERNET.
+  // connection cannot be made.
   iceServers: [
+    // 您的第一个TURN服务器
+    {
+      urls: [
+        'turn:23.95.61.186:3478?transport=udp',
+        'turn:23.95.61.186:3478?transport=tcp'
+      ],
+      username: 'h3Z7TPYyGFRkeA1',
+      credential: 'Pumhav-mamhyb-jufsa1'
+    },
+    // 您的第二个TURN服务器
+    {
+      urls: [
+        'turn:206.189.39.100:3478?transport=udp',
+        'turn:206.189.39.100:3478?transport=tcp'
+      ],
+      username: 'h3Z7TPYyGFRkeA1',
+      credential: 'Pumhav-mamhyb-jufsa1'
+    },
+    // 您也可以保留原有的服务器作为额外备份
     {
       urls: 'turn:relay1.expressturn.com:3478',
       username: 'efQUQ79N77B5BNVVKF',
-      credential: 'N4EAUgpjMzPLrxSS',
+      credential: 'N4EAugpjMzPLrxSS',
     },
   ],
+  
+  // 可选的高级配置
+  iceCandidatePoolSize: 10, // 预生成的ICE候选数量，提高连接速度
+  bundlePolicy: 'balanced'  // 媒体包策略
 }
